6a3f77857929c80751aaab5c248c36d3f9c3a25d
varasaidatta-git <varasaidatta@gmail.com>
2025-02-26 15:59:20 +0530

    a new sample added

---
+++ sample3.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+const DataFetcher = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.example.com/data')
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  return (
+    <ul>
+      {data.map(item => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default DataFetcher;
---
+++ sample5.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Name:</label>
+        <input type="text" name="name" value={formData.name} onChange={handleChange} />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input type="email" name="email" value={formData.email} onChange={handleChange} />
+      </div>
+      <div>
+        <label>Message:</label>
+        <textarea name="message" value={formData.message} onChange={handleChange}></textarea>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default ContactForm;
